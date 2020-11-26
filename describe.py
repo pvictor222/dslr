8dd9017fd945ef17abaac1626b6ab8df50f4100b
pvictor <p.victor@outlook.fr>
2020-11-26 15:24:46 +0100

    protection against wrong inputs (directory instead of a file)

--- describe.py
+++ describe.py
@@ -13,11 +13,14 @@ from src_describe.print_tables import print_tables
 """
 if __name__ == '__main__':
     if (len(sys.argv) > 1):
-        data = read_data(sys.argv[1])
-        if (data):
-            headers = data.pop(0)
-            values_dict = get_values(headers, data)
-            describe_dict = calc_description(headers, values_dict)
-            print_tables(describe_dict)
-    else:
-        print("Please provide the path of the dataset as an argument")
+        try:
+            data = read_data(sys.argv[1])
+            if (data):
+                headers = data.pop(0)
+                values_dict = get_values(headers, data)
+                describe_dict = calc_description(headers, values_dict)
+                print_tables(describe_dict)
+            else:
+                print("Please provide the path of the dataset as an argument")
+        except:
+            print("Please provide the path of the dataset as an argument")
---
+++ histogram.py
@@ -0,0 +1,17 @@
+import sys
+import plotly.graph_objects as go
+from src_describe.read_data import read_data
+
+"""
+
+"""
+if __name__ == '__main__':
+    if (len(sys.argv) > 1):
+        try:
+            data = read_data(sys.argv[1])
+            if (data):
+                print(data)
+            else:
+                print("Please provide the path of the dataset as an argument")
+        except:
+            print("Please provide the path of the dataset as an argument")
