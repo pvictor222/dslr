2348b08d356b9563ec8ea8f05131d536e48e562c
pvictor <p.victor@outlook.fr>
2020-12-02 23:21:58 +0100

    Starting logreg: reading files

--- logreg_predict.py
+++ logreg_predict.py
@@ -1,3 +1,20 @@
-"""
+import sys
+from src_describe.read_data import read_data
+from src_describe.get_values import get_values
+
 
 """
+    GOAL: 
+"""
+if __name__ == '__main__':
+    if (len(sys.argv) > 2 and sys.argv[1].lower().endswith('.csv') and sys.argv[2].lower().endswith('.txt')):
+        try:
+            data = read_data(sys.argv[1])
+            if (data):
+                print(data)
+            else:
+                print("Please provide the path of the dataset and the weights of the features as arguments")
+        except ValueError:
+            print("Please provide the path of the dataset and the weights of the features as arguments")
+    else:
+        print("Please provide the path of the dataset and the weights of the features as arguments")
