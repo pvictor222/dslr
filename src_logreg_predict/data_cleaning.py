84e2f3bd4d6fcbd5ca873ddd94173115293ec761
Paul VICTOR <p.victor@outlook.fr>
2021-01-28 09:57:03 +0100

    Started cleaning the data

--- src_logreg_predict/data_cleaning.py
+++ src_logreg_predict/data_cleaning.py
@@ -1,10 +1,24 @@
 """
     To do : 
     - Normalization
-    - Replace left/right by 0/1 ?
-    - Date of birth : split in three variables --> day, month, year?
+    - Removing names
+    - Replace left/right 1/0
+    - Date of birth : split in three variables --> day, month, year
 """
 
 def data_cleaning(data):
-    for i in data:
+    data[0].append("Year")
+    data[0].append("Month")
+    data[0].append("Day")
+    for i in data[1:]:
+        i.pop(2)
+        i.pop(2)
+        if (i[3] == "Left"):
+            i[3] = 1
+        else:
+            i[3] = 0
+        birthday = i[2].split('-')
+        i.append(birthday[0])
+        i.append(birthday[1])
+        i.append(birthday[2])
         print(i)
