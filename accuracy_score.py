7d977e9c689bdd1ff85f8904cd7110fd8aee9996
Paul VICTOR <p.victor@outlook.fr>
2021-02-07 12:46:38 +0100

    accuracy score done

--- accuracy_score.py
+++ accuracy_score.py
@@ -1,12 +1,22 @@
 import sys
+from src_describe.read_data import read_data
 import numpy as np
 from sklearn.metrics import accuracy_score
 
 if __name__ == '__main__':
     if (len(sys.argv) > 2 and sys.argv[1].lower().endswith('.csv') and sys.argv[2].lower().endswith('.csv')):
-        y_pred = [0, 2, 1, 3]
-        y_true = [0, 1, 2, 3]
-        accuracy = accuracy_score(y_true, y_pred)
-        print("The accuracy of the predictions is: " + str(round(accuracy * 100)) + "%")
+        try:
+            y_pred = [elem[1] for elem in read_data(sys.argv[1])[1:]]
+            y_true = [elem[1] for elem in read_data(sys.argv[2])[1:]]
+            if (y_pred and y_true):
+                if (len(y_pred) == len(y_true)):
+                    accuracy = accuracy_score(y_true, y_pred)
+                    print("The accuracy of the predictions is: " + str(round(accuracy * 100)) + "%")
+                else:
+                    print("The numbers of values are different")
+            else:
+                print("Values are invalid 1")
+        except:
+            print("Values are invalid 2")
     else:
         print("Please provide the path of the predictions and the real values")
